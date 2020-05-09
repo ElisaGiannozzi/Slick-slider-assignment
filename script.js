72d96d65f9487ac440d00d8bc4e7313cc65b4182
ElisaGiannozzi <elisa.giannozzi@gmail.com>
2020-05-09 20:48:52 +0200

    completed slider

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ $(document).ready(function(){
     $('.slider-container').slick({
         arrows: true,
         dots: true,
-    });
+        prevArrow: '<button class="arrow prev-arrow"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button class="arrow next-arrow"><i class="fas fa-chevron-right"></i></button>'
   });
+});
